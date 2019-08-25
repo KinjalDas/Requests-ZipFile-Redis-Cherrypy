5dcd818a7eece414285b6c8fc74222d4e41b1b53
Kinjal Das <kinjal.das86@gmail.com>
2019-08-25 23:43:24 +0530

    Second Commit -  Reading and showing table done

--- web_app.py
+++ web_app.py
@@ -1,4 +1,4 @@
-import cherrypy, redis, os
+import cherrypy, redis, os, json
 
 class HelloWorld(object):
 	@cherrypy.expose
@@ -12,31 +12,21 @@ class DBValuesGenerator(object):
 		self.records = int(self.redisclient.get("records"))
 	@cherrypy.tools.accept(media='text/plain')
 	def GET(self,records=10):
+		records = int(records)
 		list_records = []
 		limit = self.records
 		if records>limit:
 			records = limit
-		code_list = []
-		name_list = []
-		open_list = []
-		high_list = []
-		low_list = []
-		close_list = []
-		record_dict = {}
-		for i in range(self.records):
-			code_list.append(int(self.redisclient.hget(i,'code')))
-			name_list.append(self.redisclient.hget(i,'name').decode())
-			open_list.append(float(self.redisclient.hget(i,'open')))
-			high_list.append(float(self.redisclient.hget(i,'high')))
-			low_list.append(float(self.redisclient.hget(i,'low')))
-			close_list.append(float(self.redisclient.hget(i,'close')))
-		record_dict['codes'] = code_list 
-		record_dict['names'] = name_list
-		record_dict['opens'] = open_list
-		record_dict['highs'] = high_list
-		record_dict['lows'] = low_list
-		record_dict['closes'] = close_list
-		return record_dict
+		for i in range(records):
+			record_dict = {}
+			record_dict['code'] = int(self.redisclient.hget(i,'code'))
+			record_dict['name'] = self.redisclient.hget(i,'name').decode()
+			record_dict['open'] = float(self.redisclient.hget(i,'open'))
+			record_dict['high'] = float(self.redisclient.hget(i,'high'))
+			record_dict['low'] = float(self.redisclient.hget(i,'low'))
+			record_dict['close'] = float(self.redisclient.hget(i,'close'))
+			list_records.append(record_dict)
+		return json.dumps(list_records)
 
 	def POST(self,search_string):
 		code_list = []
@@ -60,7 +50,7 @@ class DBValuesGenerator(object):
 		record_dict['highs'] = high_list
 		record_dict['lows'] = low_list
 		record_dict['closes'] = close_list
-		return record_dict
+		return json.dumps(record_dict)
 
 if __name__ == '__main__':
 	conf = {
