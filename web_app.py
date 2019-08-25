d0ed0e88aeb9811e04c5d4c37ad47b8cc015d2c1
Kinjal Das <kinjal.das86@gmail.com>
2019-08-26 00:44:18 +0530

    SEARCH FUNCTION WORKING

--- web_app.py
+++ web_app.py
@@ -29,28 +29,18 @@ class DBValuesGenerator(object):
 		return json.dumps(list_records)
 
 	def POST(self,search_string):
-		code_list = []
-		name_list = []
-		open_list = []
-		high_list = []
-		low_list = []
-		close_list = []
-		record_dict = {}
+		list_records = []
 		for i in range(self.records):
 			if self.redisclient.hget(i,'name').decode().find(search_string) != -1:
-				code_list.append(int(self.redisclient.hget(i,'code')))
-				name_list.append(self.redisclient.hget(i,'name').decode())
-				open_list.append(float(self.redisclient.hget(i,'open')))
-				high_list.append(float(self.redisclient.hget(i,'high')))
-				low_list.append(float(self.redisclient.hget(i,'low')))
-				close_list.append(float(self.redisclient.hget(i,'close')))
-		record_dict['codes'] = code_list 
-		record_dict['names'] = name_list
-		record_dict['opens'] = open_list
-		record_dict['highs'] = high_list
-		record_dict['lows'] = low_list
-		record_dict['closes'] = close_list
-		return json.dumps(record_dict)
+				record_dict = {}
+				record_dict['code'] = int(self.redisclient.hget(i,'code'))
+				record_dict['name'] = self.redisclient.hget(i,'name').decode()
+				record_dict['open'] = float(self.redisclient.hget(i,'open'))
+				record_dict['high'] = float(self.redisclient.hget(i,'high'))
+				record_dict['low'] = float(self.redisclient.hget(i,'low'))
+				record_dict['close'] = float(self.redisclient.hget(i,'close'))
+				list_records.append(record_dict)
+		return json.dumps(list_records)
 
 if __name__ == '__main__':
 	conf = {
